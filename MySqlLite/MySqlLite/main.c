ff6b4c7f399f9e77aa692aec6604b3e571ea1261
Thomas <thomasfouan@hotmail.fr>
2016-06-22 19:52:06 +0200

    Initial commit. Add empty main.c

---
+++ MySqlLite/MySqlLite/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MySqlLite
+//
+//  Created by Thomas Fouan on 22/06/2016.
+//  Copyright © 2016 Thomas Fouan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
